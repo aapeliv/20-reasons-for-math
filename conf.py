e3b6bd60636ab736de89b0388ae0ceceb1a16ee9
Aapeli <git@aapelivuorinen.com>
2019-01-28 00:09:28 +1100

    Use AutoStructify to do docs

--- conf.py
+++ conf.py
@@ -1,4 +1,5 @@
 from recommonmark.parser import CommonMarkParser
+from recommonmark.transform import AutoStructify
 
 project = u'20 methods of the data scientist and the mathematics behind them'
 copyright = u'2019, Aapeli Vuorinen and Yoni Nazarathy'
@@ -26,3 +27,10 @@ language = None
 exclude_patterns = [u'_build', 'Thumbs.db', '.DS_Store']
 
 html_static_path = ['_static']
+
+def setup(app):
+    app.add_config_value('recommonmark_config', {
+            'url_resolver': lambda url: github_doc_root + url,
+            'auto_toc_tree_section': 'Contents',
+            }, True)
+    app.add_transform(AutoStructify)
